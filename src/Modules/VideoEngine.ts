9a91a5635afbf6bdff68bc47810035d09a320b5e
池ノ谷優太 <you1207.ike@gmail.com>
2023-01-25 23:01:57 +0900

    requestAnimationFrameによる更新処理(クラス版)作成

--- src/Modules/VideoEngine.ts
+++ src/Modules/VideoEngine.ts
@@ -2,47 +2,12 @@
  * このクラスは動画関連の操作をするクラス(データ保持をしてはいけない)
  */
 
-
-let time: number = 0;
-let timeRatio: number = 1;
-var animationId: number;
-
-function updateTimeRatio() {
-	const lastTime = time;
-	if (lastTime > 0) {
-		// 1フレーム当たりの時間(ミリ秒)
-		const FPS_60_SEC = 1000 / 60;
-		// 差分時間をセット
-		const dTime = new Date().getTime() - lastTime;
-		// FPS60との比較係数をセット
-		timeRatio = dTime / FPS_60_SEC;
-	}
-	// 現在時間をセット
-	time = new Date().getTime();
-}
-
-function tick() {
-	animationId = window.requestAnimationFrame(tick);
-	// 比較係数を更新
-	updateTimeRatio();
-
-	console.log(timeRatio);
-
-	//console.log("更新");
-
-	// メッシュを移動
-	//mesh.position.x += 10 * timeRatio;
-
-	// 描画
-	//renderer.render(scene, camera);
-}
-
 class VideoEngine {
 
 	private static instance: VideoEngine;	//自身のシングルトンインスタンス
-	//private time: number = 0;
-	//private timeRatio: number = 1;
-	//private animationId: number;	// requestAnimationFrameを管理するID
+	private time: number = 0;
+	private timeRatio: number = 1;
+	private animationId: number;	// requestAnimationFrameを管理するID
 
 	//コンストラクタを外部から作れないように
 	private constructor() {
@@ -55,7 +20,7 @@ class VideoEngine {
     return VideoEngine.instance;
 	}
 
-	/*public updateTimeRatio() {
+	public updateTimeRatio() {
 		const lastTime = this.time;
 		if (lastTime > 0) {
 			// 1フレーム当たりの時間(ミリ秒)
@@ -70,7 +35,7 @@ class VideoEngine {
 	}
 	
 	public tick() {
-		window.requestAnimationFrame(this.tick);
+		this.animationId = window.requestAnimationFrame(this.tick.bind(this));
 		// 比較係数を更新
 		//this.updateTimeRatio();
 	
@@ -83,13 +48,13 @@ class VideoEngine {
 	
 		// 描画
 		//renderer.render(scene, camera);
-	}*/
+	}
 
 
 	//プレビュー再生の開始
 	public startPreviewVideo() {
 		//requestAnimationFrameを開始
-		animationId = window.requestAnimationFrame(tick);
+		this.animationId = window.requestAnimationFrame(this.tick.bind(this));
 		//console.log(this.animationId);
 		//tick();
 
@@ -97,7 +62,7 @@ class VideoEngine {
 
 	public stopPreviewVideo() {
 		//requestAnimationFrameを終了
-		window.cancelAnimationFrame(animationId);
+		window.cancelAnimationFrame(this.animationId);
 	}
 
 }
